b4eab16e2f9104c808102081420892a2ef6c3db3
Camilo Higuita <chiguitar@unal.edu.co>
2025-04-28 10:29:05 -0500

    add allowCustomStyling preference entry. fix service typo

--- lib/src/modules/thememanager.h
+++ lib/src/modules/thememanager.h
@@ -15,15 +15,15 @@ class QDBusInterface;
  */
 namespace MauiMan
 {
-    
+
     class SettingsStore;
-    
+
     /**
      * @brief The ThemeManager class
      * Helpful for third parties to connect to property changes from the Theme module setting changes.
-     * 
+     *
      * @note Note that the properties can be reset back to their default values by using the `undefined` value from QML, or using the reset methods accordingly.
-     * 
+     *
      * The preferences have a set of default values, however the values used will be the ones present in the conf file. When a property is reset, then the default value will be written to the conf file.
      */
     class MAUIMAN_EXPORT ThemeManager : public QObject
@@ -39,80 +39,85 @@ namespace MauiMan
          * - 5 Inverted (True White)
          */
         Q_PROPERTY(int styleType READ styleType WRITE setStyleType NOTIFY styleTypeChanged)
-        
+
         /**
          * The preferred accent color used for the highlighted and checked states.
          */
         Q_PROPERTY(QString accentColor READ accentColor WRITE setAccentColor NOTIFY accentColorChanged RESET resetAccentColor)
-        
+
         /**
          * The preferred icon theme. This preference is only valid when using the Maui Shell session.
          */
         Q_PROPERTY(QString iconTheme READ iconTheme WRITE setIconTheme NOTIFY iconThemeChanged)
-        
+
         /**
          * The preferred theme for the CSD window control buttons. The list of available options depends on the installed themes. For more information on this refer to the MauiKit CSDControls documentation.
          */
         Q_PROPERTY(QString windowControlsTheme READ windowControlsTheme WRITE setWindowControlsTheme NOTIFY windowControlsThemeChanged)
-        
+
         /**
          * Whether to use client side decorations (CSD) for the applications.
          * By default this is set to `true`
          */
         Q_PROPERTY(bool enableCSD READ enableCSD WRITE setEnableCSD NOTIFY enableCSDChanged)
-        
+
         /**
          * The corner border radius of the UI elements, also affects the radius of the CSD window corners.
          */
         Q_PROPERTY(uint borderRadius READ borderRadius WRITE setBorderRadius NOTIFY borderRadiusChanged RESET resetBorderRadius)
-        
+
         /**
          * The preferred size of the icons in the buttons, menu entries and other elements.
          */
         Q_PROPERTY(uint iconSize READ iconSize WRITE setIconSize NOTIFY iconSizeChanged RESET resetIconSize)
-        
+
         /**
          * The preferred padding size for the UI elements, such as buttons, tool bars, menu entries, etc.
          */
         Q_PROPERTY(uint paddingSize READ paddingSize WRITE setPaddingSize NOTIFY paddingSizeChanged RESET resetPaddingSize)
-        
+
         /**
          * The preferred margin size around views.
          */
         Q_PROPERTY(uint marginSize READ marginSize WRITE setMarginSize NOTIFY marginSizeChanged RESET resetMarginSize)
-        
+
         /**
          * The preferred spacing size between elements in a row or column, such as lists or browsing elements.
          */
         Q_PROPERTY(uint spacingSize READ spacingSize WRITE setSpacingSize NOTIFY spacingSizeChanged RESET resetSPacingSize)
-        
+
         /**
          * Whether the user prefers for the system to have visual effects, such as animations, blur, etc.
          */
         Q_PROPERTY(bool enableEffects READ enableEffects WRITE setEnableEffects NOTIFY enableEffectsChanged)
-        
+
         /**
          * The preferred default font, for most part of the UI.
          */
         Q_PROPERTY(QString defaultFont READ defaultFont WRITE setDefaultFont NOTIFY defaultFontChanged RESET resetDefaultFont)
-        
+
         /**
          * The preferred small font, for details.
          */
         Q_PROPERTY(QString smallFont READ smallFont WRITE setSmallFont NOTIFY smallFontChanged RESET resetSmallFont)
-        
+
         /**
          * The preferred mono spaced font, for example the one used in console terminals, or in text editors.
          */
         Q_PROPERTY(QString monospacedFont READ monospacedFont WRITE setMonospacedFont NOTIFY monospacedFontChanged RESET resetMonospacedFont)
-        
+
         /**
          * The preferred color scheme to be used when the `styleType` is set to Custom.
          */
         Q_PROPERTY(QString customColorScheme READ customColorScheme WRITE setCustomColorScheme NOTIFY customColorSchemeChanged)
-        
+
+        /**
+         * Allow third party styles for QtQiUickControls. By default this is set to `false`. MauiKit-Style is the best fitted style to be used with Maui Apps, if you want to use a custom style, this needs to be dedicated, since existing styles won't look good with the MauiKit custom controls.
+         */
+        Q_PROPERTY(bool allowCustomStyling READ allowCustomStyling WRITE setAllowCustomStyling NOTIFY allowCustomStylingChanged)
+
     public:
-        
+
         /**
          * @brief The Theme module default values
          */
@@ -123,7 +128,7 @@ namespace MauiMan
              */
             static int preferredStyleType()
             {
-                
+
                 #ifdef Q_OS_ANDROID
                 return 1;
                 #endif
@@ -135,7 +140,7 @@ namespace MauiMan
                 #endif
                 return 0;
             }
-            
+
             /**
              * @private
              */
@@ -147,7 +152,7 @@ namespace MauiMan
                 font.setStyleName(QStringLiteral("Regular"));
                 return font.toString();
             }
-            
+
             /**
              * @private
              */
@@ -157,10 +162,10 @@ namespace MauiMan
                 font.setStyleHint(QFont::SansSerif);
                 font.setStyle(QFont::StyleNormal);
                 font.setStyleName(QStringLiteral("Regular"));
-                
+
                 return font.toString();
             }
-            
+
             /**
              * @private
              */
@@ -170,10 +175,10 @@ namespace MauiMan
                 font.setStyleHint(QFont::Monospace);
                 font.setStyle(QFont::StyleNormal);
                 font.setStyleName(QStringLiteral("Regular"));
-                
+
                 return font.toString();
             }
-            
+
             static inline const int styleType = ThemeManager::DefaultValues::preferredStyleType();
             static inline const QString accentColor = QStringLiteral("#26c6da");
             static inline const QString iconTheme = QStringLiteral("Luv");
@@ -189,64 +194,68 @@ namespace MauiMan
             static inline const QString smallFont = getSmallFont();
             static inline const QString monospacedFont = getMonospacedFont();
             static inline const QString customColorScheme = QStringLiteral("Nitrux");
+            static inline const bool allowCustomStyling = false;
         };
-        
+
         explicit ThemeManager(QObject * parent = nullptr);
-        
+
         int styleType() const;
         void setStyleType(int newStyleType);
-        
+
         const QString &accentColor() const;
         void setAccentColor(const QString &newAccentColor);
         void resetAccentColor();
-        
+
         const QString &iconTheme() const;
         void setIconTheme(const QString &newIconTheme);
-        
+
         const QString &windowControlsTheme() const;
         void setWindowControlsTheme(const QString &newWindowControlsTheme);
-        
+
         bool enableCSD() const;
         void setEnableCSD(bool enableCSD);
-        
+
         uint borderRadius() const;
         void setBorderRadius(uint newBorderRadius);
         void resetBorderRadius();
-        
+
         uint iconSize() const;
         void setIconSize(uint newIconSize);
         void resetIconSize();
-        
+
         bool enableEffects() const;
         void setEnableEffects(bool enableEffects);
-        
+
         uint paddingSize() const;
         void setPaddingSize(uint paddingSize);
         void resetPaddingSize();
-        
+
         uint marginSize() const;
         void setMarginSize(uint marginSize);
         void resetMarginSize();
-        
+
         uint spacingSize() const;
         void setSpacingSize(uint spacingSize);
         void resetSPacingSize();
-        
+
         QString defaultFont() const;
         void setDefaultFont(const QString &defaultFont);
         void resetDefaultFont();
-        
+
         QString smallFont() const;
         void setSmallFont(const QString &smallFont);
         void resetSmallFont();
-        
+
         QString monospacedFont() const;
         void setMonospacedFont(const QString &monospacedFont);
         void resetMonospacedFont();
-        
+
         QString customColorScheme() const;
         void setCustomColorScheme(const QString &customColorScheme);
-        
+
+        bool allowCustomStyling() const;
+        void setAllowCustomStyling(bool value);
+
     private Q_SLOTS:
         void onStyleTypeChanged(const int &newStyleType);
         void onAccentColorChanged(const QString &newAccentColor);
@@ -263,7 +272,7 @@ namespace MauiMan
         void onSmallFontChanged(const QString &font);
         void onMonospacedFontChanged(const QString &font);
         void onCustomColorSchemeChanged(const QString &scheme);
-        
+
     Q_SIGNALS:
         void styleTypeChanged(int styleType);
         void accentColorChanged(QString accentColor);
@@ -278,16 +287,17 @@ namespace MauiMan
         void spacingSizeChanged(uint spacingSize);
         void defaultFontChanged(QString defaultFont);
         void smallFontChanged(QString smallFont);
-        void monospacedFontChanged(QString monospacedFont);    
+        void monospacedFontChanged(QString monospacedFont);
         void customColorSchemeChanged(QString customColorScheme);
-        
+        void allowCustomStyling(bool customStyling);
+
     private:
         #if !defined Q_OS_ANDROID
         QDBusInterface *m_interface = nullptr;
         #endif
-        
+
         MauiMan::SettingsStore *m_settings;
-        
+
         int m_styleType = ThemeManager::DefaultValues::styleType;
         QString m_accentColor = ThemeManager::DefaultValues::accentColor;
         QString m_iconTheme = ThemeManager::DefaultValues::iconTheme;
@@ -303,11 +313,12 @@ namespace MauiMan
         QString m_smallFont = MauiMan::ThemeManager::DefaultValues::smallFont;
         QString m_monospacedFont = MauiMan::ThemeManager::DefaultValues::monospacedFont;
         QString m_customColorScheme = MauiMan::ThemeManager::DefaultValues::customColorScheme;
-        
+        bool m_allowCustomStyling = MauiMan::ThemeManager::DefaultValues::allowCustomStyling;
+
         void sync(const QString &key, const QVariant &value);
         void setConnections();
         void loadSettings();
-        
+
     };
 }
 
